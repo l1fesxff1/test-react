bbc885e1a677e77d20fcfce1947ac567afc2d5d6
lifesxff <lifesxff@wishdesk.com>
2023-12-28 16:02:58 +0200

    3

--- src/components/Metatags.jsx
+++ src/components/Metatags.jsx
@@ -31,10 +31,10 @@ function Metatags({type , data, viewUrl}){
                 {type !== "view" && type !== "content" && siteInfo && (
                     <title>{`${siteInfo.name} | ${siteInfo.slogan}`}</title>
                 )}
-                {ogImageUrl && <meta id="og-image" property="og:image" content={ogImageUrl} />}
-                {ogImageUrl && <meta id="og-image-url" property="og:image:url" content={ogImageUrl} />}
-                {ogImageUrl && <meta property="og:image:width" content="600" />}
-                {ogImageUrl && <meta property="og:image:height" content="400" />}
+                <meta id="og-image" property="og:image" content={"https://media.istockphoto.com/id/1432956286/uk/%D1%84%D0%BE%D1%82%D0%BE/%D0%B2%D0%B8%D0%B4-%D0%B7-%D0%B2%D0%B8%D1%81%D0%BE%D1%82%D0%B8-%D0%BF%D1%82%D0%B0%D1%88%D0%B8%D0%BD%D0%BE%D0%B3%D0%BE-%D0%BF%D0%BE%D0%BB%D1%8C%D0%BE%D1%82%D1%83-%D0%BD%D0%B0-%D0%BA%D1%80%D0%B0%D1%81%D0%B8%D0%B2%D1%96-%D0%B0%D0%BF%D0%B5%D0%BB%D1%8C%D1%81%D0%B8%D0%BD%D0%BE%D0%B2%D1%96-%D0%B4%D0%B5%D1%80%D0%B5%D0%B2%D0%B0-%D0%BD%D0%B0-%D0%BF%D0%B0%D0%B3%D0%BE%D1%80%D0%B1%D1%96-%D1%96-%D0%B3%D0%BE%D1%80%D0%B8-%D0%B2-%D0%BD%D0%B8%D0%B7%D1%8C%D0%BA%D0%B8%D1%85.jpg?s=2048x2048&w=is&k=20&c=J8sfvzKNxJJBlFWJ7MPbzKdcHCMkGnTnkwU4ePq_vrg="} />
+                <meta id="og-image-url" property="og:image:url" content={"https://media.istockphoto.com/id/1432956286/uk/%D1%84%D0%BE%D1%82%D0%BE/%D0%B2%D0%B8%D0%B4-%D0%B7-%D0%B2%D0%B8%D1%81%D0%BE%D1%82%D0%B8-%D0%BF%D1%82%D0%B0%D1%88%D0%B8%D0%BD%D0%BE%D0%B3%D0%BE-%D0%BF%D0%BE%D0%BB%D1%8C%D0%BE%D1%82%D1%83-%D0%BD%D0%B0-%D0%BA%D1%80%D0%B0%D1%81%D0%B8%D0%B2%D1%96-%D0%B0%D0%BF%D0%B5%D0%BB%D1%8C%D1%81%D0%B8%D0%BD%D0%BE%D0%B2%D1%96-%D0%B4%D0%B5%D1%80%D0%B5%D0%B2%D0%B0-%D0%BD%D0%B0-%D0%BF%D0%B0%D0%B3%D0%BE%D1%80%D0%B1%D1%96-%D1%96-%D0%B3%D0%BE%D1%80%D0%B8-%D0%B2-%D0%BD%D0%B8%D0%B7%D1%8C%D0%BA%D0%B8%D1%85.jpg?s=2048x2048&w=is&k=20&c=J8sfvzKNxJJBlFWJ7MPbzKdcHCMkGnTnkwU4ePq_vrg="} />
+                <meta property="og:image:width" content="600" />
+                <meta property="og:image:height" content="400" />
 
                 {description && type === "content" && <meta name="description" content={description} />}
 
